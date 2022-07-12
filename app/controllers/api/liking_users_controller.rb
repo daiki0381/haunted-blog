649e4d59039b8389a7cf792057256ca5c25a8497
daiki0381 <daiki03810218@gmail.com>
2022-07-12 20:42:07 +0900

    🐛 Fix No personal information is included in the returned value of the API

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,7 +3,7 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id)
+    @users = blog.liking_users.order(:id).each { |user| user.email = nil }
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
   end
